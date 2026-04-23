1579959003765bbcd97173e29b6d129ad219e14e
Aaqeb11 <aaqebahmed@gmail.com>
2026-04-23 17:38:14 +0530

    fix: malformed global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,1 @@
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elemName: string]: any;
-  }
-}
+declare module "*.css";
